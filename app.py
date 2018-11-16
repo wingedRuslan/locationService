f3c34d877b7f5ed8ad288dfaa36a4d1ca2080330
wingedRuslan <yermakovruslan@gmail.com>
2018-11-16 23:18:45 +0200

    Web app shows home page and /about page

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 from flask import Flask, render_template
 
+# instance of the Flask class
 app = Flask(__name__)
 
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

ee85f190be9ee9cb1e6381b91a05c7cb88537ef8
wingedRuslan <yermakovruslan@gmail.com>
2018-11-17 21:17:00 +0200

    Establish connection to the PostgreSQL database - pilot

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from models import db, User
 
 # instance of the Flask class
 app = Flask(__name__)
+
+# app use the pilot db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pilot'
+db.init_app(app)
 
 
 @app.route("/")

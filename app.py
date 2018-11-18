e661c6c3bce383c9f848013b1230182253002562
wingedRuslan <yermakovruslan@gmail.com>
2018-11-18 16:08:37 +0200

    Built the authentication system

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from models import db, User
-from forms import SignupForm
+from forms import SignupForm, LoginForm
 
 # instance of the Flask class
 app = Flask(__name__)
@@ -11,6 +11,7 @@ db.init_app(app)
 
 # to protect against Cross Sight Request Forgery
 app.secret_key = "development-key"
+
 
 @app.route("/")
 def index():
@@ -33,10 +34,45 @@ def signup():
             newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
             db.session.add(newuser)
             db.session.commit()
-            return "Success!"
+
+            session["email"] = newuser.email
+            return redirect(url_for("home"))
 
     elif request.method == "GET":
         return render_template("signup.html", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+
+    if request.method == "POST":
+        if form.validate() == False:
+            return render_template("login.html", form=form)
+        else:
+            email = form.email.data
+            password = form.password.data
+
+            user = User.query.filter_by(email=email).first()
+            if user is not None and user.check_password(password):
+                session["email"] = form.email.data
+                return redirect(url_for("home"))
+            else:
+                return redirect(url_for("login"))
+
+    elif request.method == "GET":
+        return render_template('login.html', form=form)
+
+
+@app.route("/logout")
+def logout():
+    session.pop("email", None)
+    return redirect(url_for("index"))
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 
 if __name__ == "__main__":

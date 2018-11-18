d05ed926bd36fb52716fed799751f00fa1edada2
wingedRuslan <yermakovruslan@gmail.com>
2018-11-18 16:31:40 +0200

    Built authorization system

--- app.py
+++ app.py
@@ -25,6 +25,9 @@ def about():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if "email" in session:
+        return redirect(url_for("home"))
+
     form = SignupForm()
 
     if request.method == "POST":
@@ -44,6 +47,9 @@ def signup():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if "email" in session:
+        return redirect(url_for("home"))
+
     form = LoginForm()
 
     if request.method == "POST":
@@ -72,6 +78,9 @@ def logout():
 
 @app.route("/home")
 def home():
+    if "email" not in session:
+        return redirect(url_for("login"))
+
     return render_template("home.html")
 
 
